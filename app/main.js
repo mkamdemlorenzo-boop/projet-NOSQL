62d2ffefcbc3e9da25ab861867340bd2f00dc655
yanis lamliji <yanis.lamliji@ynov.com>
2026-07-12 16:58:49 +0200

    Add page and other things

--- app/main.js
+++ app/main.js
@@ -20,13 +20,13 @@ const [mongo, redis, mysql, neo4j] = await Promise.all([
 ]);
     await mysql.execute("create table if not exists user (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, profile_picture VARCHAR(50))")
     await mysql.execute("create table if not exists game (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, profile_picture VARCHAR(50))")
-    await mongo.collection("game").insertOne({
-        nom: "Honkai Impact 3rd",
-        genre: ["Action et RPG"],
-        desc: "L'ultime expérience anime d'action qui devient réelle ! Des ombrages celluloïdes HD, des combos infinis, des retours explosifs... Plongez dans l'action en temps réel de dernière génération ! Une histoire originale sur toutes les lèvres, des voix de premier choix... Entrez dans la légende !",
-        suport: ["PC", "Mobile"],
-        prix: 0.00
-    });
+    // await mongo.collection("game").insertOne({
+    //     name: "Honkai Impact 3rd",
+    //     genre: ["Action et RPG"],
+    //     desc: "L'ultime expérience anime d'action qui devient réelle ! Des ombrages celluloïdes HD, des combos infinis, des retours explosifs... Plongez dans l'action en temps réel de dernière génération ! Une histoire originale sur toutes les lèvres, des voix de premier choix... Entrez dans la légende !",
+    //     support: ["PC", "Mobile"],
+    //     prix: 0.00
+    // });
 
 server.use(express.urlencoded({ extended: true }));
 
@@ -41,22 +41,165 @@ server.get("/addgame", (req, res) => {
 server.post("/addgame", async (req, res) => {
     const name = req.body.name;
     const support = req.body.support;
-    await mongo.collection("game").insertOne({
-        name: name,
-        genre: [""],
-        desc: "",
-        support: [""],
-        prix: 0.00
-    });
-    await mysql.execute(
-        "INSERT INTO game(name) VALUES (?)",
-        [name]
-    );
-    res.redirect("/");
+    const editor = req.body.editor;
+    const genre = req.body.genre;
+    const desc = req.body.desc;
+    const prix = parseInt(req.body.prix);
+    if (name != "" || support != "" || genre != "" || desc != "" || editor != "")
+    {
+        if (typeof prix === "number") {
+            await mongo.collection("game").insertOne({
+                name: name,
+                editor: editor,
+                genre: [genre],
+                desc: desc,
+                support: [support],
+                prix: prix
+            });
+            await mysql.execute(
+                "INSERT INTO game(name) VALUES (?)",
+                [name]
+            );
+            res.redirect("/game");
+        }else{
+            res.redirect("/addgame");
+        }
+    }else{
+        res.redirect("/addgame");
+    }
 });
 
 server.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "templates", "register.html"));
+});
+
+server.get("/user", async (req, res) => {
+    const [rows] = await mysql.execute("SELECT name, id FROM user");
+
+    let html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Users</title>
+    </head>
+    <body>
+        <h1>Liste des utilisateurs</h1>
+        <ul>
+    `;
+
+    for (const user of rows) {
+    html += `
+        <form action="/profile/${user.id}" method="GET">
+            <button type="submit">${user.name}</button>
+        </form>
+    `;
+}
+
+    html += `
+        </ul>
+    <form action="/register">
+        <button type="submit">create user</button>
+    </form>
+    </body>
+    </html>
+    `;
+
+    res.send(html);
+});
+
+server.get("/profile/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const [rows] = await mysql.execute(
+        "SELECT name FROM user WHERE id = ?",
+        [id]
+    );
+    const user = rows[0]
+    let html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Users</title>
+    </head>
+    <body>
+        <h1>Name: ${user.name}</h1>
+        <ul>
+    `;
+
+    for (const user of rows) {
+    }
+
+    html += `
+        </ul>
+    <form action="/friend">
+        <button type="submit">create link</button>
+    </form>
+    </body>
+    </html>
+    `;
+
+    res.send(html);
+});
+
+server.get("/game", async (req, res) => {
+    const [rows] = await mysql.execute("SELECT name, id FROM game");
+
+    let html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Games</title>
+    </head>
+    <body>
+        <h1>Liste des utilisateurs</h1>
+        <ul>
+    `;
+
+    for (const game of rows) {
+    html += `
+        <form action="/resume/${game.id}" method="GET">
+            <button type="submit">${game.name}</button>
+        </form>
+    `;
+}
+
+    html += `
+        </ul>
+    <form action="/addgame">
+        <button type="submit">create game</button>
+    </form>
+    </body>
+    </html>
+    `;
+
+    res.send(html);
+});
+
+server.get("/resume/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const [rows] = await mysql.execute(
+        "SELECT name FROM game WHERE id = ?",
+        [id]
+    );
+
+    const detail = await mongo.collection("game").findOne({name: rows[0].name}); //, editor: rows[0].editor 
+    let html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Games</title>
+    </head>
+    <body>
+        <h1>Name: ${detail.name}</h1>
+        <h1>Editor: ${detail.editor}</h1>
+        <h1>description: ${detail.desc}</h1>
+        <h1>genre: ${detail.genre[0]}</h1>
+        <h1>support: ${detail.support[0]}</h1>
+        <h1>prix: ${detail.prix}</h1>
+        <ul>
+    `;
+    res.send(html);
 });
 
 server.post("/register", async (req, res) => {
@@ -71,7 +214,7 @@ server.post("/register", async (req, res) => {
             [name, password]
         );
 
-        res.redirect("/");
+        res.redirect("/user");
     }
     res.redirect("/register");
 });
